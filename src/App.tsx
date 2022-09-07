d78ed3f67b6e29f5199e22beaa60691771e9273d
Xu Zehong <1229248506@qq.com>
2022-09-07 14:59:35 +0800

    feat(tailwind): css

--- src/App.tsx
+++ src/App.tsx
@@ -11,46 +11,50 @@ import Redux from './components/redux'
 import { Outlet, Link, useLocation, useNavigate } from 'react-router-dom'
 
 export default function App() {
-    const [num, setNum] = useState(0)
-    // const doSomething = useCallback(() => setNum((num) => num + 1), [])
-    const doSomething = useMemo(() => {
-        return () => setNum((num) => num + 1)
-    }, [])
-    const location = useLocation()
-    console.log(location.pathname)
-    const push = useNavigate()
-    function ToHome() {
-        push('/home', {
-            state: {
-                name: 'asfaf',
-            },
-        })
-    }
-    function ToAbout() {
-        push('/about')
-    }
-    function ToUser() {
-        push('/user/123')
-    }
+  const [num, setNum] = useState(0)
+  // const doSomething = useCallback(() => setNum((num) => num + 1), [])
+  const doSomething = useMemo(() => {
+    return () => setNum((num) => num + 1)
+  }, [])
+  const location = useLocation()
+  console.log(location.pathname)
+  const push = useNavigate()
+  function ToHome() {
+    push('/home', {
+      state: {
+        name: 'asfaf',
+      },
+    })
+  }
+  function ToAbout() {
+    push('/about')
+  }
+  function ToUser() {
+    push('/user/123')
+  }
 
-    return (
-        <>
-            <Outlet />
-            <button onClick={ToHome}>home</button>
-            <button onClick={ToAbout}>about</button>
-            <button onClick={ToUser}>user</button>
-            <h1>{num}</h1>
-            <button onClick={() => setNum((num) => num + 1)}>更新父组件视图</button>
-            <State />
-            <Effect />
-            <Props num={num} setNum={setNum} />
-            <Provider value={{ num, setNum }}>
-                <Context />
-            </Provider>
-            <Usecontext />
-            <Input />
-            <Memo doSomething={doSomething} />
-            <Redux />
-        </>
-    )
+  return (
+    <>
+      <Outlet />
+      <button
+        onClick={ToHome}
+        className="rounded-lg  bg-blue-500 p-2 text-white">
+        home
+      </button>
+      <button onClick={ToAbout}>about</button>
+      <button onClick={ToUser}>user</button>
+      <h1>{num}</h1>
+      <button onClick={() => setNum((num) => num + 1)}>更新父组件视图</button>
+      <State />
+      <Effect />
+      <Props num={num} setNum={setNum} />
+      <Provider value={{ num, setNum }}>
+        <Context />
+      </Provider>
+      <Usecontext />
+      <Input />
+      <Memo doSomething={doSomething} />
+      <Redux />
+    </>
+  )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from './App'
 import { Provider } from 'react-redux'
 import store from './store'
 import Router from './router'
+import './assets/css/tailwind.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
